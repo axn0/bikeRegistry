6e3a4aa3d336672c43a33006a37b2e671de917f7
April Nguyen <april_nguyen@sfu.ca>
2024-05-31 20:58:44 -0700

    Exception handling

--- src/ca/sfu/cmpt213/as1/Bike.java
+++ src/ca/sfu/cmpt213/as1/Bike.java
@@ -4,10 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class BikeRegistry {
-    
+/**
+ * Represent a bike registry.
+ * This class provides methods to manipulate the bike registry
+ * @author April Nguyen
+ * @version 1.0
+ */
+public class Bike {
+    private final int id;
+    private String owner;
+    private String type;
+    private String serial;
+    private String brake;
+    private String wheel;
+
+    private static int idCounter = 1;
     private static List<Bike> bikes = new ArrayList<>();
-    public void displayTitle(String title) {
+    private static int totalBikes = 0;
+    public static int getTotalBikes(){return totalBikes;}
+
+    public void setOwner(String owner){
+        this.owner = owner;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setSerial(String serial){
+        this.serial = serial;
+    }
+    public void setBrake(String brake){
+        this.brake = brake;
+    }
+    public void setWheel(String wheel){
+        this.wheel = wheel;
+    }
+
+    public static void displayTitle(String title) {
         String str = "* " + title + " *";
         int len = str.length();
         String border = "*".repeat(len);
@@ -15,7 +47,7 @@ public class BikeRegistry {
         System.out.println(str);
         System.out.println(border);
     }
-    public void displayMainMenu() {
+    public static void displayMainMenu() {
         System.out.println();
         displayTitle("Main Menu");
         System.out.println("1. List Bikes");
@@ -25,8 +57,10 @@ public class BikeRegistry {
         System.out.println("5. DEBUG: Dump objects (toString)");
         System.out.println("6. Exit");
     }
-    public static void selectOption() {
+
+    public static String selectOption() {
         Scanner scanner = new Scanner(System.in);
+        displayMainMenu();
         System.out.print("> ");
         String option = scanner.next();
         boolean run = true;
@@ -51,12 +85,150 @@ public class BikeRegistry {
                     run = false;
                     break;
                 default:
-                    System.out.println("Invalid input. Try again.");
+                    System.out.println("Invalid option. Try again.");
+                    displayMainMenu();
                     System.out.print("> ");
                     option = scanner.next();
             }
         }
+        return option;
     }
-    public BikeRegistry() {
+
+    public static void displayAllBikes() {
+        System.out.println(bikes.size());
+        displayTitle("List of Bikes:");
+        System.out.printf("%-3s %-16s %-10s %-11s %-7s %-9s\n", "ID", "Owner,", "Type,","Serial,","Brake","WheelSize");
+        for (Bike b : bikes) {
+            System.out.printf("%-3s %-16s %-10s %-11s %-7s %-9s\n", b.id + ",", b.owner + ",", b.type + ",", b.serial + ",", b.brake + ",", b.wheel);
+        }
+    }
+
+    public static Bike findBike(int id) {
+        for (Bike b : bikes) {
+            if (b.id == id) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    public static void addNewBike() {
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        String owner;
+        String type;
+        String serial;
+        String brake;
+        String wheel;
+        System.out.printf("%-30s", "Enter Bike owner name:");
+        owner = scanner.next();
+        System.out.printf("%-30s", "Enter Bike type:");
+        type = scanner.next();
+        System.out.printf("%-30s", "Enter Bike's serial number:");
+        serial = scanner.next();
+        System.out.printf("%-30s", "Enter Bike's brake type:");
+        brake = scanner.next();
+        System.out.printf("%-30s", "Enter Bike's wheel type:");
+        wheel = scanner.next();
+        Bike newBike = new Bike(owner,type,serial,brake,wheel);
+        bikes.add(newBike);
+    }
+
+    public static void deleteBike() {
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        int id;
+        displayAllBikes();
+        System.out.println("Enter ID (0 to cancel)");
+        System.out.print("> ");
+        id = Integer.parseInt(scanner.next());
+        if (id == 0) {
+            return;
+        }
+        Bike b = findBike(id);
+        if (b == null) {
+            System.out.println("Bike not found.");
+            System.out.println("Returning to menu.");
+        }
+        else {
+            bikes.remove(b);
+            System.out.println("Removed!");
+        }
+    }
+
+    public static void alterBike() {
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        int id;
+        String attribute;
+        String newValue;
+        System.out.println("Enter ID (0 to cancel)");
+        System.out.print("> ");
+        id = Integer.parseInt(scanner.next());
+        if (id == 0) {
+            return;
+        }
+        Bike b = findBike(id);
+        if (b == null) {
+            System.out.println("Bike not found.");
+            System.out.println("Returning to menu.");
+        }
+        else {
+            System.out.println("Which Attribute?");
+            System.out.print("> ");
+            attribute = scanner.next();
+            System.out.println("New Value");
+            System.out.print("> ");
+            newValue = scanner.next();
+            switch (attribute) {
+                case "Owner":
+                    b.owner = newValue;
+                    break;
+                case "Type":
+                    b.type = newValue;
+                    break;
+                case "Serial":
+                    b.serial = newValue;
+                    break;
+                case "Brake":
+                    b.brake = newValue;
+                    break;
+                case "Wheel Size":
+                    b.wheel = newValue;
+                    break;
+                default:
+                    System.out.println("Invalid Attribute");
+            }
+            System.out.println("Saved!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        String location = this.getClass().getName();
+        int _id = this.id;
+        String id = "[Id:" + _id + ", ";
+        String owner = "Owner:" + this.owner + ", ";
+        String type = "type:" + this.type + ", ";
+        String serial = "Serial:" + this.serial + ", ";
+        String brake = "Brake:" + this.brake + ", ";
+        String wheel = "WheelSize:" + this.wheel + "]";
+        return location + id + owner + type + serial + brake + wheel;
+    }
+
+    public static void dumpAllBikes() {
+        int i = 1;
+        for (Bike b : bikes) {
+            System.out.println(i + "." + b.toString());
+            i++;
+        }
+    }
+
+    public Bike(String owner, String type, String serial, String brake, String wheel) {
+        this.id = idCounter;
+        idCounter++;
+        this.owner = owner;
+        this.type = type;
+        this.serial = serial;
+        this.brake = brake;
+        this.wheel = wheel;
+        totalBikes++;
     }
 }
--- src/ca/sfu/cmpt213/as1/Main.java
+++ src/ca/sfu/cmpt213/as1/Main.java
@@ -1,13 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        BikeRegistry menu = new BikeRegistry();
-        menu.printTitle("Bike Registry by April Nguyen sn 301314527");
-        System.out.println("\n");
-        menu.printTitle("Main Menu");
-        menu.printMenu();
-        menu.selectOption();
+package ca.sfu.cmpt213.as1;
 
+/**
+ * Provides the main functionality of a bike registry with options
+ * to list, add, remove, change, and dump objects the bikes
+ * @author April Nguyen
+ *  @version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Bike.displayTitle("Bike Registry by April Nguyen sn 301314527");
+        boolean exit = false;
+        while(!exit) {
+            String option = Bike.selectOption();
+            switch (option) {
+                case "1":
+                    if (Bike.getTotalBikes() > 0) {
+                        Bike.displayAllBikes();
+                    }
+                    break;
+                case "2":
+                    Bike.addNewBike();
+                    break;
+                case "3":
+                    try {
+                        Bike.deleteBike();
+                    }
+                    catch (Exception e) {
+                        System.out.println("Error: " + e.getMessage());
+                        System.out.println("Invalid option. Try again.");
+                    }
+                    break;
+                case "4":
+                    try {
+                        Bike.alterBike();
+                    }
+                    catch (Exception e) {
+                        System.out.println("Error: " + e.getMessage());
+                        System.out.println("Invalid option. Try again.");
+                    }
+                    break;
+                case "5":
+                    Bike.dumpAllBikes();
+                    break;
+                case "6":
+                    System.out.println("BYE!");
+                    exit = true;
+                default:
+                    System.out.println("Invalid option. Try again.");
+                    break;
+            }
+        }
     }
 }
